a8fae134762bdfed4dfd4653112fbe05cf39bd33
DasAneesh <149708161+DasAneesh@users.noreply.github.com>
2024-01-19 19:57:13 +0300

    init

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -5,11 +5,11 @@ const Product = ({ fotocars }) => {
 
   return (
     <div
-      className="flex flex-col bg-slate-500 hover:bg-red-100 w-40 aspect-video"
+      className="flex flex-col bg-slate-500 hover:bg-red-100 w-96 aspect-video"
       onClick={() => navigate(`/details/${fotocars.id}`)}
     >
       <img src={fotocars.img_src} alt="car" />
-      <div className="">
+      <div className="text-xl">
         <p>Модель: {fotocars.name}</p>
         <p>Цена: {fotocars.price} руб.</p>
         <p>Год выпуска: {fotocars.year}</p>
--- src/layouts/ProductLayout.jsx
+++ src/layouts/ProductLayout.jsx
@@ -3,13 +3,13 @@ import { Outlet } from "react-router-dom";
 const ProductLayout = () => {
   // ...
   return (
-    <main className="flex h-screen flex-col bg-yellow-200 gap-4">
+    <main className="flex min-h-screen flex-col bg-yellow-200 gap-4">
       <div className="flex h-40 w-full justify-center items-center  bg-[#b89067] ">
         <a
           href="/details"
           className="flex justify-center font-serif font-bold text-xl"
         >
-          <h2>Получить карточку автомобиля.</h2>
+          <h2>Нажми на карточку автомобиля.</h2>
         </a>
       </div>
       <Outlet />
