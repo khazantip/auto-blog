4b9ee25698e7c7a56ec95e678fc1fad08771a7bb
DasAneesh <149708161+DasAneesh@users.noreply.github.com>
2024-01-19 19:37:55 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ProductLayout from "./layouts/ProductLayout";
+import FotoCar from "./pages/HomePages";
+import DetailsPages from "./pages/DetailsPages";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProductLayout />,
+    children: [
+      {
+        path: "/",
+        element: <FotoCar />,
+      },
+      {
+        path: "/details/:id",
+        element: <DetailsPages />,
+      },
+    ],
+  },
+  //...
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
